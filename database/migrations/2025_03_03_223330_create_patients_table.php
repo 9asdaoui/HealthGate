ab6848497f63bcaa887e202700884e45aa5809be
9asdaoui <oussamaqasdaoui@gmail.com>
2025-03-03 22:52:03 +0000

    remove unused AdminSeeder; update migrations for Doctors, Patients, Departments, and DoctorPatientRelations with foreign keys and additional fields

--- database/migrations/2025_03_03_223330_create_patients_table.php
+++ database/migrations/2025_03_03_223330_create_patients_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->float('height');
+            $table->float('weight');
+            $table->date('date_of_birth');
             $table->timestamps();
         });
     }
--- database/migrations/2025_03_03_223342_create_doctor_patient_relations_table.php
+++ database/migrations/2025_03_03_223342_create_doctor_patient_relations_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('doctor_patient_relations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->float('height');
+            $table->float('weight');
+            $table->date('date_of_birth');
             $table->timestamps();
         });
     }
