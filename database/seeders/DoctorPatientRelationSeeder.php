430ba96bb90b2868970b27ff00377e0fcac6a431
9asdaoui <oussamaqasdaoui@gmail.com>
2025-03-03 22:34:07 +0000

    add models and seeders for Admin, Doctor, Patient, Department, and DoctorPatientRelation; create corresponding factories and migrations

---
+++ database/seeders/DoctorPatientRelationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DoctorPatientRelationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
