c957b3a387b9c3b6b4d160c0098691ecb5a9ca6b
9asdaoui <oussamaqasdaoui@gmail.com>
2025-04-20 04:38:35 +0100

    feat: Add request validation for authentication - Implement StoreloginRequest and StoreRegisterRequest for login and registration validation, and create ChangePasswordRequest for password changes.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangePasswordRequest;
+use App\Http\Requests\StoreloginRequest;
+use App\Http\Requests\StoreRegisterRequest;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,24 +17,21 @@ class AuthController extends Controller
     {
         return view('auth.login');
     }
-    
+
     public function showRegistrationForm()
     {
         return view('auth.register');
     }
 
-    public function login(Request $request)
+    public function login(StoreloginRequest $request)
     {
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             $user = Auth::user();
-            
-            switch($user->role->name){
+
+            switch ($user->role->name) {
                 case 'admin':
                     return redirect()->route('admin.dashboard');
                 case 'doctor':
@@ -49,22 +49,12 @@ class AuthController extends Controller
     }
 
 
-    public function register(Request $request)
+    public function register(StoreRegisterRequest $request)
     {
-        $validated = $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-            'gender' => 'required|in:male,female',
-            'height' => 'required|numeric|nullable',
-            'weight' => 'required|numeric|nullable',
-            'date_of_birth' => 'required|date|nullable',
-            'role_id' => 'integer|exists:roles,id',
-        ]);
+        $validated = $request->validated();
 
         $roleId = Role::where('name', 'patient')->first()->id;
-        // dd($roleId);
+
         $user = User::create([
             'first_name' => $validated['first_name'],
             'last_name' => $validated['last_name'],
@@ -73,6 +63,7 @@ class AuthController extends Controller
             'gender' => $validated['gender'],
             'role_id' => $roleId,
         ]);
+
         $user->patient()->create([
             'height' => $validated['height'],
             'weight' => $validated['weight'],
@@ -84,24 +75,22 @@ class AuthController extends Controller
         return redirect()->route('patient.dashboard');
     }
 
-    public function changePassword(Request $request)
+    public function changePassword(ChangePasswordRequest $request)
     {
         try {
-            $request->validate([
-                'current_password' => 'required',
-                'new_password' => 'required|min:8|confirmed',
-                'new_password_confirmation' => 'required'
-            ]);
+            $validated = $request->validated();
 
-        $user = Auth::user();
+            $user = Auth::user();
 
-        if (!Hash::check($request->current_password, $user->password)) {
-            return back()->withErrors(['current_password' => 'Current password is incorrect']);
-        }
+            if (!Hash::check($validated['current_password'], $user->password)) {
+                return back()->withErrors(['current_password' => 'Current password is incorrect']);
+            }
 
-        $user->password = $request->new_password;
-        $user->update;
-
+            $user->password = $validated['new_password'];
+            /**
+             * @var User $user
+             */
+            $user->update();
         } catch (\Exception $e) {
             return back()->withErrors(['error' => $e->getMessage()]);
         }
@@ -114,5 +103,4 @@ class AuthController extends Controller
         Auth::logout();
         return redirect('/');
     }
-
 }
